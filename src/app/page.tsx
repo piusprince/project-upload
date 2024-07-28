c9a34cf74d010bfcd60813808e3b8575c998aa56
Pius Prince <45078705+piusprince@users.noreply.github.com>
2024-07-28 14:57:55 +0000

    feat: Set up home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,30 @@ import { useMutation, useQuery } from "convex/react";
 import Image from "next/image";
 import { api } from "../../convex/_generated/api";
 import { Button } from "@/components/ui/button";
+import { useOrganization, useUser } from "@clerk/nextjs";
 
 export default function Home() {
+  const { organization, isLoaded } = useOrganization();
+  const { user, isLoaded: userIsLoaded } = useUser();
+
+  let orgId: string | undefined = undefined;
+
+  if (isLoaded && userIsLoaded) {
+    orgId = organization?.id ?? user?.id;
+  }
+
   const addProject = useMutation(api.projects.createProject);
-  const projects = useQuery(api.projects.getProjects);
+  const projects = useQuery(
+    api.projects.getProjects,
+    orgId ? { orgId } : "skip"
+  );
+
+  console.log({ id: organization?.id });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {projects &&
-        projects.length > 0 &&
-        projects.map((project) => (
+        projects?.map((project) => (
           <div
             key={project._id}
             className="flex flex-col items-center justify-center gap-4 p-4"
@@ -22,7 +36,15 @@ export default function Home() {
           </div>
         ))}
 
-      <Button onClick={() => addProject({ name: "test" })}>Add project</Button>
+      <Button
+        onClick={() => {
+          console.log({ orgId });
+          if (!orgId) return;
+          addProject({ name: "test", orgId });
+        }}
+      >
+        Add project
+      </Button>
     </main>
   );
 }
