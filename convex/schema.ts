d0b511139fe0e32792ac68e0e667c7eec9892a59
Pius Prince <45078705+piusprince@users.noreply.github.com>
2024-07-28 14:59:01 +0000

    feat: Setup schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,5 +2,12 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-  projects: defineTable({ name: v.string() }),
+  projects: defineTable({
+    name: v.string(),
+    orgId: v.optional(v.string()),
+  }).index("by_orgId", ["orgId"]),
+  students: defineTable({
+    tokenIdentifier: v.string(),
+    orgIds: v.array(v.string()),
+  }).index("by_tokenIdentifier", ["tokenIdentifier"]),
 });
