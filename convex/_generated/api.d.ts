850bd305cb68d241c58fe3251996402109841ee5
Pius Prince <45078705+piusprince@users.noreply.github.com>
2024-07-28 15:00:16 +0000

    feat: Convex generated api

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,7 +14,10 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as clerk from "../clerk.js";
+import type * as http from "../http.js";
 import type * as projects from "../projects.js";
+import type * as students from "../students.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -25,7 +28,10 @@ import type * as projects from "../projects.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  clerk: typeof clerk;
+  http: typeof http;
   projects: typeof projects;
+  students: typeof students;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
